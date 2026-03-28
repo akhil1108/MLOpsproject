925e1694f7b33c3f61bd3d68aabae31348ff363b
akhil1108 <mathakhilesh@gmail.com>
2026-03-28 10:33:31 +0000

    first commit

---
+++ tour_project/hosting/hosting.py
@@ -0,0 +1,10 @@
+from huggingface_hub import HfApi
+import os
+
+api = HfApi(token=os.getenv("HF_TOKEN"))
+api.upload_folder(
+    folder_path="tour_project/deployment",     # the local folder containing your files
+    repo_id="Akhilesh1108/Project1",          # the target repo
+    repo_type="space",                      # dataset, model, or space
+    path_in_repo="",                          # optional: subfolder path inside the repo
+)
